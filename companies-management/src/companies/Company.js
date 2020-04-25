8f2eec27a55721e6f984f221d54fece171b1543f
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2020-04-25 20:42:23 +0200

    Use hooks

--- companies-management/src/companies/Company.js
+++ companies-management/src/companies/Company.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import './Company.scss';
 
-export class Company extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <button className="company" onClick={() => this.props.onEdit()}>
-        <h2 className="company-name">{this.props.name}</h2>
-        <div className="company-details">
-          <p className="company-date">
-            First purchase: <strong>{this.props.dateOfFirstPurchase}</strong>
+export function Company(props) {
+  return (
+    <button className='company' onClick={() => props.onEdit()}>
+      <h2 className='company-name'>{props.name}</h2>
+      <div className='company-details'>
+        <p className='company-date'>
+          First purchase: <strong>{props.dateOfFirstPurchase}</strong>
+        </p>
+        <div className='company-budget'>
+          <p>
+            <strong>Budget summary</strong>
           </p>
-          <div className="company-budget">
-            <p>
-              <strong>Budget summary</strong>
-            </p>
-            <p>Total: {this.props.budget}€</p>
-            <p>Spent: {this.props.budgetSpent}€</p>
-            <p>Left: {this.props.budgetLeft}€</p>
-          </div>
+          <p>Total: {props.budget}€</p>
+          <p>Spent: {props.budgetSpent}€</p>
+          <p>Left: {props.budgetLeft}€</p>
         </div>
-      </button>
-    );
-  }
+      </div>
+    </button>
+  );
 }
--- companies-management/src/companies/CompanyEditor.js
+++ companies-management/src/companies/CompanyEditor.js
@@ -1,73 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CompanyEditor.scss';
 import { validateBudget } from './validateBudget';
 
-export class CompanyEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFormValid: true,
-      budget: props.company.budget
-    };
-    this.handleBudgetChange = this.handleBudgetChange.bind(this);
-  }
+export function CompanyEditor(props) {
+  const [isFormValid, setFormValid] = useState(true);
+  const [budget, setBudget] = useState(props.company.budget);
 
-  handleBudgetChange(e) {
+  function handleBudgetChange(e) {
     const newBudget = e.target.value;
-    this.setState((state, props) => ({
-      budget: newBudget,
-      isFormValid: validateBudget(newBudget, props.company)
-    }));
+    setBudget(newBudget);
+    setFormValid(validateBudget(newBudget, props.company));
   }
 
-  renderErrorMsg() {
+  function renderErrorMsg() {
     return (
       <small className='error-message'>
-        Make sure that budget is a number and it is not less than already spent ({this.props.company.budgetSpent}€).
+        Make sure that budget is a number and it is not less than already spent ({props.company.budgetSpent}€).
       </small>
     );
   }
 
-  renderButtons() {
+  function renderButtons() {
     return (
       <div className='buttons'>
-        <button className='button button--secondary' type='button' onClick={this.props.onCancel}>
+        <button className='button button--secondary' type='button' onClick={props.onCancel}>
           Cancel
         </button>
         <button
-          className={`button button--primary ${!this.state.isFormValid && 'button--disabled'}`}
+          className={`button button--primary ${!isFormValid && 'button--disabled'}`}
           type='button'
-          disabled={this.state.isFormValid}>
+          disabled={isFormValid}>
           Save
         </button>
       </div>
     );
   }
 
-  render() {
-    return (
-      <>
-        <h2>{this.props.company.name}</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}>
-          <div className={`form-control ${!this.state.isFormValid && 'form-control--invalid'}`}>
-            <label htmlFor='budget'>Edit budget</label>
-            <input
-              autoFocus='autofocus'
-              autoComplete='none'
-              name='budget'
-              id='budget'
-              type='text'
-              value={this.state.budget}
-              onChange={this.handleBudgetChange}
-            />
-            {!this.state.isFormValid && this.renderErrorMsg()}
-          </div>
-          {this.renderButtons()}
-        </form>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>{props.company.name}</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}>
+        <div className={`form-control ${!isFormValid && 'form-control--invalid'}`}>
+          <label htmlFor='budget'>Edit budget</label>
+          <input
+            autoFocus='autofocus'
+            autoComplete='none'
+            name='budget'
+            id='budget'
+            type='text'
+            value={budget}
+            onChange={handleBudgetChange}
+          />
+          {!isFormValid && renderErrorMsg()}
+        </div>
+        {renderButtons()}
+      </form>
+    </>
+  );
 }
